47522e67ae38a12df9e353c84092de8b75faba12
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2024-05-06 13:40:27 +0200

    feat: initial commit

---
+++ src/test/java/com/example/kostplan/MainTests.java
@@ -0,0 +1,11 @@
+package com.example.kostplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+	@Test
+	void contextLoads() {
+	}
+}
