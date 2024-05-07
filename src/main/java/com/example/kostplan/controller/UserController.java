9a3bc367d908e3498eab0ae97c7e4ff7f031d2c2
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2024-05-07 14:51:27 +0200

    feat: add a route for /login

---
+++ src/main/java/com/example/kostplan/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.kostplan.controller;
+
+import com.example.kostplan.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
+
+@Controller
+public class UserController {
+	private final UserService service;
+
+	@Autowired
+	public UserController(UserService service) {
+		this.service = service;
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
