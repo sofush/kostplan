319a4f97f4134ae23f83047a85d78c0e39cf34bd
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2024-05-11 14:13:34 +0200

    refactor: remove unused import

--- src/main/java/com/example/kostplan/service/UserService.java
+++ src/main/java/com/example/kostplan/service/UserService.java
@@ -10,7 +10,6 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.xml.crypto.Data;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.temporal.ChronoUnit;
