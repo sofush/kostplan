b66011b447bd0717c419e4bd1503f27c35451f66
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2024-06-02 15:56:36 +0200

    feat: update PersistentStorage method declarations to include throws-clause

--- src/main/java/com/example/kostplan/service/PersistentStorage.java
+++ src/main/java/com/example/kostplan/service/PersistentStorage.java
@@ -1,18 +1,19 @@
 package com.example.kostplan.service;
 
 import com.example.kostplan.entity.*;
+import org.springframework.dao.DataAccessException;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface PersistentStorage {
-	User findUserByUsername(String username);
-	void addUser(User user);
-	Recipe findRecipeById(int id);
-	List<Recipe> findRecipesForWeek(int weekIndex);
-	void addDay(Day day);
-	void updateDay(Day day);
-	Day findDay(String username, LocalDate date);
-	byte[] findRecipeImage(int recipeId);
-	void addRecipeImage(int recipeId, byte[] imageBytes);
+	User findUserByUsername(String username) throws DataAccessException;
+	void addUser(User user) throws DataAccessException;
+	Recipe findRecipeById(int id) throws DataAccessException;
+	List<Recipe> findRecipesForWeek(int weekIndex) throws DataAccessException;
+	void addDay(Day day) throws DataAccessException;
+	void updateDay(Day day) throws DataAccessException;
+	Day findDay(String username, LocalDate date) throws DataAccessException;
+	byte[] findRecipeImage(int recipeId) throws DataAccessException;
+	void addRecipeImage(int recipeId, byte[] imageBytes) throws DataAccessException;
 }
