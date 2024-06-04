3f51c868921443517e68cec7c7b684107be46ef1
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2024-06-04 17:27:58 +0200

    feat: log unhandled global exceptions

--- src/main/java/com/example/kostplan/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/kostplan/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.kostplan.exception;
 
 import com.stripe.exception.StripeException;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,8 +11,15 @@ import java.nio.file.AccessDeniedException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private final Logger logger;
+
+	public GlobalExceptionHandler() {
+		this.logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+	}
+
 	@ExceptionHandler({StripeException.class})
 	public String handleStripeException(StripeException e, Model model) {
+		logger.error("Unhandled Stripe error", e);
 		model.addAttribute("title", "Fejl i Stripe");
 		model.addAttribute("content", e.getUserMessage());
 		return "error";
@@ -18,6 +27,7 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler({AccessDeniedException.class})
 	public String handleAccessDeniedException(AccessDeniedException e, Model model) {
+		logger.error("Unhandled access denied error", e);
 		model.addAttribute("title", "Adgang nægtet");
 		model.addAttribute("content", "Du har ikke adgang til denne side.");
 		return "error";
@@ -25,6 +35,7 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler({Exception.class})
 	public String handleGenericException(Exception e, Model model) {
+		logger.error("Unhandled error", e);
 		model.addAttribute("title", "Ukendt fejl");
 		model.addAttribute("content", "En ukendt fejl opstod.");
 		return "error";
