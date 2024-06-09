e425f9e5d762ffa651e9691bf8ee5d86039311e8
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2024-06-09 17:35:52 +0200

    fix: import correct Exception type in GlobalExceptionHandler

--- src/main/java/com/example/kostplan/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/kostplan/exception/GlobalExceptionHandler.java
@@ -3,12 +3,12 @@ package com.example.kostplan.exception;
 import com.stripe.exception.StripeException;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-import java.nio.file.AccessDeniedException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
